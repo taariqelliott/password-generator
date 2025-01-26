dfdb56a95cf60d5b38b2fa74a9d05fd5c4317829
Taariq Elliott <codebytaariq@gmail.com>
2025-01-26 17:48:27 -0500

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="absolute w-full bg-emerald-950 py-2">
+	<h1 class="ml-2 text-center text-lime-500">Password Generator</h1>
+</div>
+{@render children()}
+
+<div class="absolute bottom-0 flex w-full justify-between bg-emerald-950 py-2 text-lime-500">
+	<div class="ml-2 flex flex-row gap-2">
+		<a
+			href="https://github.com/taariqelliott"
+			class="hover:opacity-70 active:scale-90 active:opacity-70"
+			target="”_blank”"><Icon icon="iconoir:github" width="30" height="30" /></a
+		>
+		<a
+			href="https://www.linkedin.com/in/taariq-elliott/"
+			class="hover:opacity-70 active:scale-90 active:opacity-70"
+			target="”_blank”"
+			><Icon icon="iconoir:linkedin" width="30" height="30" />
+		</a>
+		<a
+			href="https://taariqelliott.netlify.app/"
+			class="hover:opacity-70 active:scale-90 active:opacity-70"
+			target="”_blank”"
+			><Icon icon="iconoir:code" width="30" height="30" />
+		</a>
+	</div>
+	<div class="mr-2">
+		<a href="mailto:codebytaariq@gmail.com" class="hover:underline">codebytaariq@gmail.com</a>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,286 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { letters, numbers, commonSymbols, specialSymbols } from './components/characters';
+	import { copy } from 'svelte-copy';
+
+	let selectedLength: number | null = $state(8);
+	let passwords: string[] = $state([]);
+	let includeLetters = $state(true);
+	let includeNumbers = $state(true);
+	let includeCommonSymbols = $state(true);
+	let includeSpecialSymbols = $state(false);
+	let isLowerCase = $state(false);
+	let isUpperCase = $state(false);
+	let noSpaces = $state(false);
+	let popupVisible = $state(false);
+	let mountedPassword = $state('');
+	let password = $state(' ');
+	let inputElement: HTMLInputElement;
+	// let popupText = $state('');
+
+	function generatePassword() {
+		passwords = [];
+
+		if (
+			selectedLength === null ||
+			(!includeLetters && !includeNumbers && !includeCommonSymbols && !includeSpecialSymbols)
+		) {
+			for (let idx = 0; idx < 5; idx++) {
+				passwords.push(' ');
+			}
+			return;
+		}
+
+		let availableChars: (string | number)[] = [];
+		if (includeLetters) availableChars.push(...letters);
+		if (includeNumbers) availableChars.push(...numbers);
+		if (includeCommonSymbols) availableChars.push(...commonSymbols);
+		if (includeSpecialSymbols) availableChars.push(...specialSymbols);
+
+		for (let i = 0; i < 5; i++) {
+			password = Array.from(
+				{ length: selectedLength },
+				() => availableChars[Math.floor(Math.random() * availableChars.length)]
+			).join('');
+			passwords.push(password);
+		}
+	}
+
+	onMount(() => {
+		let availableChars: (string | number)[] = [];
+		if (includeLetters) availableChars.push(...letters);
+		if (includeNumbers) availableChars.push(...numbers);
+		if (includeCommonSymbols) availableChars.push(...commonSymbols);
+		if (includeSpecialSymbols) availableChars.push(...specialSymbols);
+
+		for (let i = 0; i < 5; i++) {
+			password = '';
+			mountedPassword = '';
+			for (let j = 0; j < 8; j++) {
+				mountedPassword += availableChars[Math.floor(Math.random() * availableChars.length)];
+			}
+			password += mountedPassword;
+			passwords.push(password);
+		}
+	});
+
+	function copyToClipboard(target: string) {
+		const element = document.getElementById(target);
+		if (element) {
+			// const copyText = element.innerText;
+			// popupText = copyText;
+			popupVisible = true;
+			setTimeout(() => {
+				popupVisible = false;
+			}, 2000);
+		}
+	}
+
+	function clearAllFields() {
+		includeLetters = false;
+		includeNumbers = false;
+		includeCommonSymbols = false;
+		includeSpecialSymbols = false;
+		isLowerCase = false;
+		isUpperCase = false;
+		noSpaces = false;
+		passwords = [];
+		for (let idx = 0; idx < 5; idx++) {
+			passwords.push(' ');
+		}
+	}
+</script>
+
+<div class="flex h-dvh flex-col items-center justify-center bg-emerald-900 text-zinc-100">
+	<div class="flex flex-col space-y-2 pb-1">
+		<div>
+			<input
+				type="checkbox"
+				name="letters"
+				bind:checked={includeLetters}
+				onchange={() => generatePassword()}
+			/>
+			<label for="letters">Letters</label>
+		</div>
+		<div>
+			<input
+				type="checkbox"
+				name="numbers"
+				bind:checked={includeNumbers}
+				onchange={() => generatePassword()}
+			/>
+			<label for="numbers">Numbers</label>
+		</div>
+		<div>
+			<input
+				type="checkbox"
+				name="commonSymbols"
+				bind:checked={includeCommonSymbols}
+				onchange={() => generatePassword()}
+			/>
+			<label for="commonSymbols">Common Symbols</label>
+		</div>
+		<div>
+			<input
+				type="checkbox"
+				name="specialSymbols"
+				bind:checked={includeSpecialSymbols}
+				onchange={() => generatePassword()}
+			/>
+			<label for="specialSymbols">Special Symbols</label>
+		</div>
+		<div>
+			<input
+				type="checkbox"
+				name="lowercase"
+				disabled={!includeLetters}
+				bind:checked={isLowerCase}
+				onchange={() => {
+					if (isUpperCase) {
+						isUpperCase = false;
+					}
+				}}
+			/>
+			<label for="lowercase">Lowercase</label>
+		</div>
+		<div>
+			<input
+				type="checkbox"
+				name="uppercase"
+				disabled={!includeLetters}
+				bind:checked={isUpperCase}
+				onchange={() => {
+					if (isLowerCase) {
+						isLowerCase = false;
+					}
+				}}
+			/>
+			<label for="uppercase">Uppercase</label>
+		</div>
+		<div>
+			<input
+				type="checkbox"
+				name="noSpaces"
+				disabled={!includeCommonSymbols}
+				bind:checked={noSpaces}
+			/>
+			<label for="noSpaces">No Spaces</label>
+		</div>
+	</div>
+
+	<select
+		name="length"
+		class="w-48 rounded border bg-lime-500 py-1 text-emerald-950"
+		bind:value={selectedLength}
+		onchange={() => generatePassword()}
+	>
+		<option value={8} disabled selected>Select length...</option>
+		{#each Array.from({ length: 443 }, (_, i) => i + 8) as length}
+			<option value={length}>{length}</option>
+		{/each}
+	</select>
+	<div class="flex flex-row gap-4">
+		<button onclick={clearAllFields}>
+			<div
+				class="mt-2 w-36 cursor-pointer rounded-lg border-2 border-lime-500 bg-zinc-800 px-2 py-3 text-zinc-100 transition-all duration-100 hover:opacity-90 active:scale-95 active:opacity-90"
+			>
+				Clear
+			</div>
+		</button>
+		<button
+			onclick={() => {
+				includeLetters = true;
+				includeCommonSymbols = true;
+				includeNumbers = true;
+				let count = 0;
+				let intervalId = setInterval(() => {
+					generatePassword();
+					count++;
+					if (count >= 200) {
+						clearInterval(intervalId);
+					}
+				}, 1);
+			}}
+		>
+			<div
+				class="mt-2 w-36 cursor-pointer rounded-lg border-2 border-lime-500 bg-zinc-800 px-2 py-3 text-zinc-100 transition-all duration-100 hover:opacity-90 active:scale-95 active:opacity-90"
+			>
+				Shuffle 🔀
+			</div>
+		</button>
+	</div>
+
+	<div class="relative mt-2 w-full max-w-sm">
+		<input
+			placeholder="Paste..."
+			type="text"
+			class="w-full flex-row justify-between gap-2 rounded-lg bg-zinc-800 px-4 py-4 text-zinc-100"
+			bind:this={inputElement}
+		/>
+		<button
+			type="button"
+			class="absolute top-1/2 right-4 -translate-y-1/2 transform text-xl text-lime-500"
+			onclick={() => (inputElement.value = '')}
+		>
+			&times;
+		</button>
+	</div>
+	<div
+		class="mt-2 flex h-[1px] w-80 max-w-sm flex-row justify-between gap-2 rounded-lg bg-lime-500 px-4 text-zinc-100"
+	></div>
+	{#each passwords as pw, index}
+		<div
+			class="mt-2 flex w-full max-w-sm flex-row justify-between gap-2 rounded-lg bg-zinc-800 px-4 py-4 text-zinc-100"
+		>
+			<div id="password-{index}" class="hide-scrollbar flex-1 overflow-x-scroll whitespace-nowrap">
+				{#if isLowerCase}
+					{#if noSpaces}
+						{pw.toLowerCase().replace(/\s/g, '')}
+					{:else}
+						{pw.toLowerCase()}
+					{/if}
+				{:else if isUpperCase}
+					{#if noSpaces}
+						{pw.toUpperCase().replace(/\s/g, '')}
+					{:else}
+						{pw.toUpperCase()}
+					{/if}
+				{:else if noSpaces}
+					{pw.replace(/\s/g, '')}
+				{:else}
+					{pw}
+				{/if}
+			</div>
+			<button
+				disabled={pw.replace(/\s/g, '').length === 0}
+				class="w-10 cursor-pointer text-center hover:opacity-90 active:scale-90 disabled:cursor-not-allowed disabled:opacity-50"
+				use:copy={{ text: pw }}
+				onclick={() => {
+					copyToClipboard(`password-${index}`);
+				}}
+			>
+				<span id="copy-{index}">📋</span>
+			</button>
+		</div>
+	{/each}
+	{#if popupVisible}
+		<div
+			class="absolute top-12 right-2 max-w-[200px] overflow-hidden rounded-lg border-2 border-emerald-950 bg-lime-500 px-2 py-3 text-xs font-bold text-ellipsis whitespace-nowrap text-emerald-950 sm:top-auto sm:bottom-14"
+		>
+			Copied!
+		</div>
+	{/if}
+</div>
+
+<style>
+	input[type='checkbox'] {
+		accent-color: #7bce00;
+	}
+	.hide-scrollbar::-webkit-scrollbar {
+		display: none;
+	}
+	.hide-scrollbar {
+		-ms-overflow-style: none;
+		scrollbar-width: none;
+	}
+</style>
