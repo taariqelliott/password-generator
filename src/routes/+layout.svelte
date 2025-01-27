245b7c80488fb404a8997fe34d260bbc360e705e
Taariq Elliott <codebytaariq@gmail.com>
2025-01-27 17:26:19 -0500

    Refactor layout styles for improved consistency and hover effects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="absolute w-full bg-emerald-950 py-2">
-	<h1 class="ml-2 text-center text-lime-500">•Password Please•</h1>
+	<h1 class="text-center text-lime-500">•Password Please•</h1>
 </div>
 {@render children()}
 
@@ -13,23 +13,23 @@
 	<div class="ml-2 flex flex-row gap-2 transition-all duration-200">
 		<a
 			href="https://github.com/taariqelliott"
-			class="transition-all duration-200 hover:opacity-70 active:scale-90 active:opacity-70"
+			class="transition-all duration-200 hover:text-fuchsia-500 hover:opacity-70 active:scale-90 active:opacity-70"
 			target="”_blank”"><Icon icon="iconoir:github" width="30" height="30" /></a
 		>
 		<a
 			href="https://www.linkedin.com/in/taariq-elliott/"
-			class="transition-all duration-200 hover:opacity-70 active:scale-90 active:opacity-70"
+			class="transition-all duration-200 hover:text-fuchsia-500 hover:opacity-70 active:scale-90 active:opacity-70"
 			target="”_blank”"
 			><Icon icon="iconoir:linkedin" width="30" height="30" />
 		</a>
 		<a
 			href="https://taariqelliott.netlify.app/"
-			class="transition-all duration-200 hover:opacity-70 active:scale-90 active:opacity-70"
+			class="transition-all duration-200 hover:text-fuchsia-500 hover:opacity-70 active:scale-90 active:opacity-70"
 			target="”_blank”"
 			><Icon icon="iconoir:code" width="30" height="30" />
 		</a>
 	</div>
-	<div class="mr-2 flex items-center justify-center text-sm hover:underline">
+	<div class="mr-2 flex items-center justify-center text-sm hover:text-fuchsia-500 hover:underline">
 		<a href="mailto:codebytaariq@gmail.com">codebytaariq@gmail.com</a>
 	</div>
 </div>
